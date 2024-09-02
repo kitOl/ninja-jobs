7923fe39525aba47926b63af2cc2f6b169b7a64f
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-09-02 20:50:47 +0300

    added routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import AboutView from '../views/AboutView.vue';
+import Jobs from '../views/jobs/Jobs.vue';
+import JobDetails from '../views/jobs/JobDetails.vue';
 
 const routes = [
   {
@@ -15,6 +17,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: AboutView,
+  },
+  {
+    path: '/jobs',
+    name: 'Jobs',
+    component: Jobs,
+  },
+  {
+    path: '/jobs/:id',
+    name: 'JobDetails',
+    component: JobDetails,
   },
 ];
 
