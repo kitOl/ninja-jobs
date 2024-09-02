27bbf874756064b90bb25afdbcf89fbda27a0a13
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-09-02 19:44:56 +0300

    Add basic files

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur
+      eaque hic, fuga soluta laborum dignissimos ullam ipsam vitae similique, in
+      sapiente est, suscipit ea laudantium ducimus. Vitae cumque quod quas
+      distinctio alias doloremque necessitatibus, nam omnis dicta ad, est magni
+      illo incidunt nemo odit rem molestias officiis eligendi sint nulla
+      aliquam? Ratione rem quisquam vitae dolorem autem minus quis veritatis
+      quod, quam dolor. Vitae, aspernatur! Ullam quaerat dicta ex similique
+      reiciendis? Hic amet, facere illo ut recusandae in! Earum quaerat saepe
+      adipisci suscipit et placeat. Vel quod incidunt magnam, pariatur
+      reprehenderit eius aliquam beatae perferendis voluptatum cupiditate
+      quibusdam mollitia quisquam?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur
+      eaque hic, fuga soluta laborum dignissimos ullam ipsam vitae similique, in
+      sapiente est, suscipit ea laudantium ducimus. Vitae cumque quod quas
+      distinctio alias doloremque necessitatibus, nam omnis dicta ad, est magni
+      illo incidunt nemo odit rem molestias officiis eligendi sint nulla
+      aliquam? Ratione rem quisquam vitae dolorem autem minus quis veritatis
+      quod, quam dolor. Vitae, aspernatur! Ullam quaerat dicta ex similique
+      reiciendis? Hic amet, facere illo ut recusandae in! Earum quaerat saepe
+      adipisci suscipit et placeat. Vel quod incidunt magnam, pariatur
+      reprehenderit eius aliquam beatae perferendis voluptatum cupiditate
+      quibusdam mollitia quisquam?
+    </p>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit incidunt
+      quos veniam libero cupiditate repellendus officiis ex, voluptas nam
+      doloribus temporibus natus corrupti aliquid commodi expedita aliquam modi
+      nihil iusto quasi pariatur perferendis. Numquam minima nostrum esse
+      asperiores repudiandae eum aspernatur a iusto optio ea qui, autem dolor
+      delectus magni voluptates velit minus, et repellat. Fugit animi illo iure
+      temporibus dicta vero obcaecati incidunt? Repudiandae beatae, illo totam
+      ab ea, quam dolorem vero vitae soluta veniam incidunt nobis necessitatibus
+      in aliquid consequuntur, natus voluptatibus iure nesciunt aperiam officiis
+      labore. Eaque reprehenderit labore officia quae esse quisquam hic, quam
+      nesciunt! Numquam?
+    </p>
+
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit incidunt
+      quos veniam libero cupiditate repellendus officiis ex, voluptas nam
+      doloribus temporibus natus corrupti aliquid commodi expedita aliquam modi
+      nihil iusto quasi pariatur perferendis. Numquam minima nostrum esse
+      asperiores repudiandae eum aspernatur a iusto optio ea qui, autem dolor
+      delectus magni voluptates velit minus, et repellat. Fugit animi illo iure
+      temporibus dicta vero obcaecati incidunt? Repudiandae beatae, illo totam
+      ab ea, quam dolorem vero vitae soluta veniam incidunt nobis necessitatibus
+      in aliquid consequuntur, natus voluptatibus iure nesciunt aperiam officiis
+      labore. Eaque reprehenderit labore officia quae esse quisquam hic, quam
+      nesciunt! Numquam?
+    </p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  components: {},
+};
 </script>
